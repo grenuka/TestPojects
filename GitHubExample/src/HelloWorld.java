c9ad151d71d6ae5b4ec3eb7e615484df40095838
rgourig <rgourig@rgourig.centurylink.com>
2015-05-11 17:19:45 +0530

    updated the changes

--- GitHubExample/src/HelloWorld.java
+++ GitHubExample/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-	System.out.println("Renuka >>>>>>>>>>>");
+	System.out.println("Renuka1aaa >>>>>>>>>>>");
 
 	}
 
--- GitHubExample/src/Sudent.java
+++ GitHubExample/src/Sudent.java
@@ -2,6 +2,6 @@
 public class Sudent {
 
 	public static void main(String[] args) {
-		System.out.println("This is student class");
+		System.out.println("This is student q1class");
 	}
 }
